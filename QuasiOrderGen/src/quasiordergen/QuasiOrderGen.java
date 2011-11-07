a66bcf4bb1ceedea3e9286e78c16babc31b95687
Siva Manoharan <smanoharan@outlook.com>
2011-11-07 23:17:05 +0000

    Implemented the isRelated checker, along with associated tests.

--- QuasiOrderGen/src/quasiordergen/QuasiOrderGen.java
+++ QuasiOrderGen/src/quasiordergen/QuasiOrderGen.java
@@ -1,8 +1,9 @@
 package quasiordergen;
+import java.util.BitSet;
 
 /**
  *
- * @author sm264
+ * @author Siva Manoharan [avismanoharan@hotmail.com]
  */
 public class QuasiOrderGen 
 {
@@ -16,8 +17,8 @@ public class QuasiOrderGen
         //  input: 
         //      Group of N elements(ArrayList<String>) ; 
         //      K Subgroups ( List<BitArray> ) ;
-        //      [Optionally] cayley table ;
-        //  
+        //      [Optionally] Cayley table ;
+
         //  process:
         //      For each element of G, create a bitArray for subgroup membership.
         //      Generate all subsets, (2^K) represent them as bitArrays
@@ -31,12 +32,36 @@ public class QuasiOrderGen
         //                  check if g1 \in S => g2 \in S [ O(1) using bitMaps ]
         //                  create Relation - add to list
         //      [Optionally] Find all unique relations from list
-        //
+
         //  output:        
         //      A list of relations 
         //      [Optionally] Convert it to a displayable form.
         //      
         
         
+    }
+
+    /**
+     * Determine if g1 <= g2 (i.e. if (g1,g2) \in Relation ) given their subgroup memberships
+     *  and the subgroup family under consideration.
+     *
+     * @param g1Mask A bitmask representing g1's subgroup membership. The i'th bit is 1 iff g1 \in subgroup_i
+     * @param g2Mask A bitmask representing g2's subgroup membership. The i'th bit is 1 iff q2 \in subgroup_i
+     * @param subgroupFamilyMask A bitmask representing the subgroup family under consideration. The i'th bit is 1 iff subgroup_i \in family
+     * @return whether g1 <= g2
+     */
+    public static boolean isRelated(BitSet g1Mask, BitSet g2Mask, BitSet subgroupFamilyMask)
+    {
+        // start with b: 1 iff g1 \in sub-i
+        BitSet b = (BitSet)g1Mask.clone();
+
+        // 'AND' with subgroup family: 1 iff ( g1 \in sub-i && sub-i \in family )
+        b.and(subgroupFamilyMask);
+
+        // 'AND' with NOT of g2Mask: 1 iff ( g1 \in sub-i && sub-i \in family  && g2 \in sub-i)
+        b.andNot(g2Mask);
+
+        // if any bits are set, the relation is false (there is a counter example)
+        return b.isEmpty();
     }
 }
---
+++ QuasiOrderGen/test/quasiordergen/QuasiOrderGenIsRelatedTest.java
@@ -0,0 +1,183 @@
+package quasiordergen;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.BitSet;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ *
+ * @author sm264
+ */
+public class QuasiOrderGenIsRelatedTest
+{
+    // 5 elements : { A, B, C, D, E }
+    final int NumElem = 5;
+    final int IA = 0;
+    final int IB = 1;
+    final int IC = 2;
+    final int ID = 3;
+    final int IE = 4;
+
+    BitSet[] elementMasks;
+
+    @Before
+    public void setUp()
+    {
+        elementMasks = new BitSet[NumElem];
+
+        // 4 subgroups : { {A}, {A,B}, {A,B,C}, {A,B,C,D,E} }
+        elementMasks[IA] = StringToBitSet("1111");   // A is in all subgroups
+        elementMasks[IB] = StringToBitSet("0111");   // B is in sub-2,3,4
+        elementMasks[IC] = StringToBitSet("0011");   // C is in sub-3 and sub-4
+        elementMasks[ID] = StringToBitSet("0001");   // D is in sub-4 only
+        elementMasks[IE] = StringToBitSet("0001");   // E is in sub-4 only
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testIsRelatedForSingletonFamilyIsACompleteGraph()
+    {
+        // family is singleton: (in this case, only the 3rd subgroup ({A,B,C}) is part of the family)
+        BitSet subgroupFamilyMask = StringToBitSet("0010");
+
+        List<Integer> inSub3 = new ArrayList<Integer>();
+        List<Integer> notInSub3 = new ArrayList<Integer>();
+        inSub3.add(IA);     // A is in sub-3
+        inSub3.add(IB);     // B is in sub-3
+        inSub3.add(IC);     // C is in sub-3
+        notInSub3.add(ID);  // D is not in sub-3
+        notInSub3.add(IE);  // E is not in sub-3
+
+        // Check that :
+        //  All elements \in sub-3 are related.
+        for (int i : inSub3)
+            for (int j : inSub3)
+                assertIsRelated(i, j, subgroupFamilyMask);
+
+        //  All elements not \in sub-3 are related.
+        for (int i : notInSub3)
+            for (int j : notInSub3)
+                assertIsRelated(i, j, subgroupFamilyMask);
+
+        //  All elements \in sub-3 are <= all elements not \in sub-3 (but not vice versa)
+        for (int i : inSub3)
+        {
+            for (int j : notInSub3)
+            {
+                assertIsRelated(j, i, subgroupFamilyMask);
+                assertIsNotRelated(i, j, subgroupFamilyMask);
+            }
+        }
+    }
+
+    @Test
+    public void TestIsRelatedRelationIsAsExpectedWhenFamilyContainsAllSubgroups()
+    {
+        // family contains all subgroups:
+        BitSet subgroupFamilyMask = StringToBitSet("1111");
+
+        // expected relations:
+        //      everything <= A
+        for (int i=0;i<NumElem;i++)
+            assertIsRelated(i, IA, subgroupFamilyMask);
+
+        //      (all but A) <= B
+        for (int i=0;i<NumElem;i++)
+            if (i == IA) assertIsNotRelated(i, IB, subgroupFamilyMask);
+            else assertIsRelated(i, IB, subgroupFamilyMask);
+
+        //      (all but A,B) <= C
+        for (int i=0;i<NumElem;i++)
+            if (i == IA || i == IB) assertIsNotRelated(i, IC, subgroupFamilyMask);
+            else assertIsRelated(i, IC, subgroupFamilyMask);
+
+        //      (all but A,B,C) <= D,E
+        for (int i=0;i<NumElem;i++)
+        {
+            if (i <= IC)
+            {
+                assertIsNotRelated(i, ID, subgroupFamilyMask);
+                assertIsNotRelated(i, IE, subgroupFamilyMask);
+            }
+            else
+            {
+                assertIsRelated(i, ID, subgroupFamilyMask);
+                assertIsRelated(i, IE, subgroupFamilyMask);
+            }
+        }
+    }
+
+    @Test
+    public void TestIsRelatedRelationIsAsExpectedWhenFamilyContainsTwoSubgroups()
+    {
+        // subgroup family: {A,B}, {A,B,C}
+        BitSet subgroupFamilyMask = StringToBitSet("0110");
+
+        // expected relations:
+        //      everything <= A,B
+        for (int i=0;i<NumElem;i++)
+        {
+            assertIsRelated(i, IA, subgroupFamilyMask);
+            assertIsRelated(i, IB, subgroupFamilyMask);
+        }
+
+        //      (all but A,B) <= C
+        for (int i=0;i<NumElem;i++)
+            if (i == IA || i == IB) assertIsNotRelated(i, IC, subgroupFamilyMask);
+            else assertIsRelated(i, IC, subgroupFamilyMask);
+
+        //      (only D,E) <= D,E
+        for (int i=0;i<NumElem;i++)
+        {
+            if (i <= IC)
+            {
+                assertIsNotRelated(i, ID, subgroupFamilyMask);
+                assertIsNotRelated(i, IE, subgroupFamilyMask);
+            }
+            else
+            {
+                assertIsRelated(i, ID, subgroupFamilyMask);
+                assertIsRelated(i, IE, subgroupFamilyMask);
+            }
+        }
+    }
+
+    private static BitSet StringToBitSet(String BitString)
+    {
+        BitSet result = new BitSet(BitString.length());
+
+        for(int i=0;i<BitString.length();i++)
+            if (BitString.charAt(i)=='1')
+                result.set(i);
+        
+        return result;
+    }
+
+    private void assertIsRelatedIsEqualTo(boolean expected, int i, int j, BitSet subgroupFamily)
+    {
+        final int offset = (int)'A';
+        char ci = (char)(i + offset);
+        char cj = (char)(j + offset);
+        String message = "family=" + subgroupFamily + "; " + ci + "<=" + cj;
+        assertEquals(message, expected, QuasiOrderGen.isRelated(elementMasks[i], elementMasks[j], subgroupFamily));
+    }
+
+    private void assertIsRelated(int i, int j, BitSet subgroupFamily)
+    {
+        assertIsRelatedIsEqualTo(true, i, j, subgroupFamily);
+    }
+
+    private void assertIsNotRelated(int i, int j, BitSet subgroupFamily)
+    {
+        assertIsRelatedIsEqualTo(false, i, j, subgroupFamily);
+    }
+}

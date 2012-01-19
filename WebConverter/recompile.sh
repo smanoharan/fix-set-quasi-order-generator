9dde89710fb2acef9d5625233230ffbaefa776bc
Siva Manoharan <smanoharan@outlook.com>
2012-01-19 23:33:26 +0000

    Reorganising files into folders

--- WebConverter/recompile.sh
+++ WebConverter/recompile.sh
@@ -1,3 +1,7 @@
-php -f to-table.php -- general-15.csv All 15 1 > data-table/all15.html
-php -f to-table.php -- selected.csv Selected 31 16 > data-table/selected.html 
-php -f to-table.php -- dih.sorted.csv Dihedral 126 1 ni > data-table/dih.html
+php -f to-table.php -- data/general-15.csv All 15 1 > out/all15.html
+php -f to-table.php -- data/selected.csv Selected 31 16 > out/selected.html 
+php -f to-table.php -- data/dih.sorted.csv Dihedral 126 1 ni > out/dih.html
+php -f to-table.php -- data/symalt.csv Symmetric/Alternating 120 1 ni > out/symalt.html 
+php -f to-latdiag.php > out/latdiag.html
+zip export/no-images.zip -q out/
+ls -l ./ export
